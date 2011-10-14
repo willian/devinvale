3fbfbc557382f349c6dd3c53c5221d8d5481ba49
Willian Fernandes <willian@willianfernandes.com.br>
2011-10-14 18:18:05 -0300

    adicionei o mailer de confirmação de cadastro

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.reset_password_email.subject
-  #
-  def reset_password_email
-    @greeting = "Hi"
+  def confirmation(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email
   end
 end
