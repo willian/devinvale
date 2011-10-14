a8102b3b6f527349d3c0c9c19a9d6dbde3a5f307
Willian Fernandes <willian@willianfernandes.com.br>
2011-10-14 13:51:41 -0300

    faltou tirar o campo password da validação

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   permalink :name, :to_param => :permalink
 
 
-  validates :name, :email, :password, :cpf, :city_name, :state_name, presence: true
+  validates :name, :email, :cpf, :city_name, :state_name, presence: true
   validates :foreigner, :ita_student, :ita_badge, inclusion: [false, true]
   validates :email, :cpf, uniqueness: true
   validates_cpf_format_of :cpf
