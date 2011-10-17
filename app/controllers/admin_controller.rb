5d47a14a527721c4eb4004ce54dff369af18a8f9
Robson Júnior <robson@kimmay.com.br>
2011-10-17 18:14:23 -0200

    secutiry on admin page :)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
+require 'digest/md5'
+
 class AdminController < ApplicationController
+
+  before_filter :authenticate
 
   def index
     @users = User.all(:order => 'id DESC')
@@ -8,6 +12,14 @@ class AdminController < ApplicationController
     @user = User.find(params[:id])
     @user.destroy
     redirect_to(admin_index_path)
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      Digest::MD5.hexdigest(username) == "28bf2e6c32b964f7261a327256774a2f" && Digest::MD5.hexdigest(password) == "fad23ce038b4a1f2a8408890231e5392"
+    end
   end
 
 end
