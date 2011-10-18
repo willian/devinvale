e430d9e3094293c868d714052439c8a4673cf832
Willian Fernandes <willian@willianfernandes.com.br>
2011-10-18 12:37:12 -0200

    adicionei um tempo para evitar problemas com o smtp do gmail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     @user.waiting_list = true
     if @user.save
-      UserMailer.delay.confirmation(@user)
+      UserMailer.delay(run_at: 1.minute.from_now).confirmation(@user)
       redirect_to root_path, notice: t("flash_messages.users.create.notice")
     else
       render action: "new"
