94ea82fee1c4439ab42fdcf00dbe07f6aad64b79
Robson Júnior <robson@kimmay.com.br>
2011-10-17 23:42:46 -0200

    waiting list page :(

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.waiting_list = true
     if @user.save
       UserMailer.delay.confirmation(@user)
       redirect_to root_path, notice: t("flash_messages.users.create.notice")
