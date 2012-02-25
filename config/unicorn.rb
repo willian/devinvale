beb85fa82c7cf83456310f1af5732dd14291fe6a
Willian Fernandes <willian@willianfernandes.com.br>
2012-02-25 13:08:29 -0200

    adicionei o unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,110 @@
+# Sample verbose configuration file for Unicorn (not Rack)
+#
+# This configuration file documents many features of Unicorn
+# that may not be needed for some applications. See
+# http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
+# for a much simpler configuration file.
+#
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+
+# Use at least one worker per core if you're on a dedicated server,
+# more will usually help for _short_ waits on databases/caches.
+rails_env = ENV["RAILS_ENV"] || "production"
+worker_processes (rails_env == "production" ? 10 : 2)
+
+# Since Unicorn is never exposed to outside clients, it does not need to
+# run on the standard HTTP port (80), there is no reason to start Unicorn
+# as root unless it's from system init scripts.
+# If running the master process as root and the workers as an unprivileged
+# user, do this to switch euid/egid in the workers (also chowns logs):
+# user "unprivileged_user", "unprivileged_group"
+
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+APP_PATH = "/var/www/devinvale/current"
+working_directory APP_PATH # available in 0.94.0+
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen APP_PATH + "/tmp/sockets/unicorn.sock", :backlog => 2048
+if rails_env == "staging"
+  listen 4000, :tcp_nopush => true
+else
+  listen 5000, :tcp_nopush => true
+end
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 60
+
+# feel free to point this anywhere accessible on the filesystem
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stdout.log"
+
+# REE - http://www.rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  # if defined?(ActiveRecord::Base)
+  #   ActiveRecord::Base.connection.disconnect!
+  # end
+
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = APP_PATH + "/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :TERM : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+      #Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+  sleep 1
+end
+
+after_fork do |server, worker|
+  if rails_env == "staging"
+    port = 4000 + worker.nr
+  else
+    port = 5000 + worker.nr
+  end
+
+  child_pid = server.config[:pid].sub('.pid', ".#{port}.pid")
+  system("echo #{Process.pid} > #{child_pid}")
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true",
+  # if defined?(ActiveRecord::Base)
+  #   ActiveRecord::Base.establish_connection
+  # end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
+
